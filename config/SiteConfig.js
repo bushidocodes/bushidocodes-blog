9fa162f0e2d868f2ee1aa93eb0855dc95de3db59
Sean McBride <spmcbride1201@gmail.com>
2019-05-13 17:29:15 -0400

    fix: correct banner file extension

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -5,7 +5,7 @@ module.exports = {
   siteTitleAlt: 'Bushido Codes - Sean McBride', // Alternative Site title for SEO
   siteUrl: 'https://bushido.codes', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteBanner: '/social/banner.png', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
   siteDescription: 'Codes with Honor, Defends the Just, Pursues Lofy Undertakings on the Cutting Edge', // Your site description
   author: 'Sean McBride', // Author for schemaORGJSONLD
