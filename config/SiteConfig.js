626788b6651ff4f3a12471998d16b1a105d620f9
Sean McBride <spmcbride1201@gmail.com>
2019-05-13 17:42:47 -0400

    chore: another attempted fix to appease validators

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-
+  
+  author: 'Sean McBride', // Author for schemaORGJSONLD
+  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
+  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteDescription: 'Codes with Honor, Defends the Just, Pursues Lofty Undertakings on the Cutting Edge', // Your site description
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
   siteTitle: 'Bushido Codes', // Navigation and Site Title
   siteTitleAlt: 'Bushido Codes - Sean McBride', // Alternative Site title for SEO
   siteUrl: 'https://bushido.codes', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteBanner: '/social/banner.png', // Your image for og:image tag. You can find it in the /static folder
-  favicon: 'src/favicon.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Codes with Honor, Defends the Just, Pursues Lofty Undertakings on the Cutting Edge', // Your site description
-  author: 'Sean McBride', // Author for schemaORGJSONLD
-  siteLogo: '/social/logo.png', // Image for schemaORGJSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
   userTwitter: '@bushidocodes', // Twitter Username - Optional
