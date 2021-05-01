f081f3c498df4f0cb310aa4db700dc4302146b5a
Sean McBride <spmcbride1201@gmail.com>
2021-05-01 17:12:23 -0400

    chore: reorder tracking ids

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,22 +67,11 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ["UA-117490231-1", "G-XYCR5NVHV6"],
-        // This object gets passed directly to the gtag config command
-        // This config will be shared across all trackingIds
-        // gtagConfig: {
-        //   optimize_id: "OPT_CONTAINER_ID",
-        //   anonymize_ip: false,
-        //   cookie_expires: 0,
-        // },
+        trackingIds: ["G-XYCR5NVHV6", "UA-117490231-1"],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
           head: false,
-          // Setting this parameter is also optional
-          respectDNT: false,
-          // Avoids sending pageview hits from custom paths
-          // exclude: [],
         },
       },
     },
