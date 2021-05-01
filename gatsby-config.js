9ecb4fb0a5094dcbb9a7372cba5579e427931e6d
Sean McBride <spmcbride1201@gmail.com>
2021-05-01 16:55:59 -0400

    chore: Add G tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,7 +67,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-        trackingIds: ["UA-117490231-1"],
+        trackingIds: ["UA-117490231-1", "G-XYCR5NVHV6"],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         // gtagConfig: {
