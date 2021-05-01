895fadb36d17dead29af3af2ddc3fffdefff0e7b
Sean McBride <spmcbride1201@gmail.com>
2021-05-01 16:41:57 -0400

    chore: Disable old Google Analytics tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,14 +86,14 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "UA-117490231-1",
-        head: true,
-        anonymize: true,
-        respectDNT: true,
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: "UA-117490231-1",
+    //     head: true,
+    //     anonymize: true,
+    //     respectDNT: true,
+    //   },
+    // },
   ],
 };
